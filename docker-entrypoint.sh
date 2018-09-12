e8b3ee40d9ce8820f32551e7f39ee0365240a423
Radek Kysely <kyselyradek@gmail.com>
2018-09-12 16:06:27 +0200

    kdb+ starts from ~/

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+AUTH_FILE="/root/_userpass.txt"
+STARTUP_FILE="/root/_startup.q"
+
 get_userpass() {
 	if [ "$1" != "" ]; then
-		USERPASS="-u /userpass.txt"
+		USERPASS="-u $AUTH_FILE"
 		return 0
 	else
 		return 1
@@ -13,14 +16,14 @@ main() {
 	declare PORT="$1" ON_STARTUP="$2" AUTH="$3"
 
 	# Set up the environment
-	echo "$ON_STARTUP" >> /server.q
-	echo "$AUTH" >> /userpass.txt
+	echo "$ON_STARTUP" >> $STARTUP_FILE
+	echo "$AUTH" >> $AUTH_FILE
 
 	# Check whether we want to set up authentication
 	get_userpass "$AUTH" || USERPASS=""
 
 	# Start up the server on given port, load /server.q
-	/root/q/l32/q /server.q $USERPASS -p 0.0.0.0:$PORT
+	cd root/ && ./q/l32/q $STARTUP_FILE $USERPASS -p 0.0.0.0:$PORT
 }
 
 main "$@"
